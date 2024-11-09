61bf38a4f3f02d23cbbd4ed28b066e18a3af7b2b
Diego Pagotto <diegodeabreupagotto@gmail.com>
2024-11-09 00:40:28 -0300

    feat: changes border color

--- src/app/components/IPCard/index.tsx
+++ src/app/components/IPCard/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card, Button, message } from 'antd';
+import './style.css';
 
 interface IPCardProps {
     title: string;
@@ -13,7 +14,11 @@ const IPCard: React.FC<IPCardProps> = ({ title, value }) => {
     };
 
     return (
-        <Card title={title} extra={<Button onClick={handleCopy}>Copy</Button>}>
+        <Card
+            title={title}
+            className="ip-card"
+            extra={<Button onClick={handleCopy}>Copy</Button>}
+        >
             <p>{value}</p>
         </Card>
     );
