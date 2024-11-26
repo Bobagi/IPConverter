bcb05533b2c4f60ba8560336778a1cd450591f05
Gustavo Perin <gustavoperin067@gmail.com>
2024-11-26 01:26:46 +0000

    style: add particles background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,73 @@
-import type { Metadata } from 'next';
-import localFont from 'next/font/local';
-import './globals.css';
+"use client";
+
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import type { Container, Engine } from "tsparticles-engine";
+import { loadSlim } from "tsparticles-slim";
+import localFont from "next/font/local";
+import "./globals.css";
 
 const geistSans = localFont({
-    src: './fonts/GeistVF.woff',
-    variable: '--font-geist-sans',
-    weight: '100 900',
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
 });
 const geistMono = localFont({
-    src: './fonts/GeistMonoVF.woff',
-    variable: '--font-geist-mono',
-    weight: '100 900',
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
 });
 
-export const metadata: Metadata = {
-    title: 'IP Converter',
-    description: 'Convert IP addresses to unusual formats.',
-};
-
 export default function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-            <body className={`${geistSans.variable} ${geistMono.variable}`}>
-                {children}
-            </body>
-        </html>
-    );
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(
+    async (container: Container | undefined) => {
+      await console.log(container);
+    },
+    []
+  );
+
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Particles
+          id="tsparticles"
+          options={{
+            particles: {
+              number: {
+                value: 100,
+                density: {
+                  enable: true,
+                  value_area: 800,
+                },
+              },
+              shape: {
+                type: "circle",
+              },
+              opacity: {
+                value: 0.5,
+              },
+              size: {
+                value: 3,
+              },
+              move: {
+                enable: true,
+                speed: 2,
+              },
+            },
+          }}
+          init={particlesInit}
+          loaded={particlesLoaded}
+        />
+        {children}
+      </body>
+    </html>
+  );
 }
---
+++ src/app/metadata.ts
@@ -0,0 +1,6 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "IP Converter",
+  description: "Convert IP addresses to unusual formats.",
+};
