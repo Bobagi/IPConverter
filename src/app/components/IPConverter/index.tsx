55acb24ad036ba80ae27c1c76e36c0a982a8c4f7
Gustavo Perin <gustavoperin067@gmail.com>
2024-11-26 01:24:19 +0000

    style: border colors and spacing

--- src/app/components/IPConverter/index.tsx
+++ src/app/components/IPConverter/index.tsx
@@ -1,49 +1,49 @@
-'use client';
-import React, { useState } from 'react';
-import { Input, Button, Card } from 'antd';
-import './style.css';
-import { convertIP } from '@/app/utils/ConvertionUtils';
-import IPCard from '../IPCard';
+"use client";
+import React, { useState } from "react";
+import { Input, Button, Card } from "antd";
+import "./style.css";
+import { convertIP } from "@/app/utils/ConvertionUtils";
+import IPCard from "../IPCard";
 
 const IPConverter: React.FC = () => {
-    const [ip, setIp] = useState<string>('');
-    const [converted, setConverted] = useState<{
-        decimal: string;
-        hexadecimal: string;
-        binary: string;
-    } | null>(null);
+  const [ip, setIp] = useState<string>("");
+  const [converted, setConverted] = useState<{
+    decimal: string;
+    hexadecimal: string;
+    binary: string;
+  } | null>(null);
 
-    const handleConvert = () => {
-        const result = convertIP(ip);
-        setConverted(result);
-    };
+  const handleConvert = () => {
+    const result = convertIP(ip);
+    setConverted(result);
+  };
 
-    return (
-        <Card className="card">
-            <Input
-                placeholder="IP to Convert (ex: 192.168.0.1)"
-                value={ip}
-                onChange={(e) => setIp(e.target.value)}
-                className="input"
-            />
-            <div className="button-container">
-                <Button
-                    type="primary"
-                    onClick={handleConvert}
-                    className="button"
-                >
-                    Do the magic!
-                </Button>
-            </div>
-            {converted && (
-                <div>
-                    <IPCard title="Decimal" value={converted.decimal} />
-                    <IPCard title="Hexadecimal" value={converted.hexadecimal} />
-                    <IPCard title="Binary" value={converted.binary} />
-                </div>
-            )}
-        </Card>
-    );
+  return (
+    <Card className="card">
+      <Input
+        placeholder="IP to Convert (ex: 192.168.0.1)"
+        value={ip}
+        onChange={(e) => setIp(e.target.value)}
+        className="input"
+      />
+      <div className="button-container">
+        <Button
+          type="primary"
+          onClick={handleConvert}
+          className="button"
+        >
+          Do the magic!
+        </Button>
+      </div>
+      {converted && (
+        <div className="converted-container">
+          <IPCard title="Decimal" value={converted.decimal} />
+          <IPCard title="Hexadecimal" value={converted.hexadecimal} />
+          <IPCard title="Binary" value={converted.binary} />
+        </div>
+      )}
+    </Card>
+  );
 };
 
 export default IPConverter;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,33 +4,33 @@ import { ConfigProvider } from 'antd';
 
 const Home: React.FC = () => {
     return (
-        <ConfigProvider
-            componentSize="large"
-            theme={{
-                token: {
-                    colorPrimary: '#FFA500',
-                    colorPrimaryBorder: '#FFA500',
-                    colorPrimaryHover: '#FFA500',
-                    colorPrimaryActive: '#FFA500',
-                    colorBgContainer: '#121212',
-                    colorBorder: '#d9d9d9',
-                    colorPrimaryBg: '#FFA500',
-                    colorText: '#d9d9d9',
-                },
-            }}
+      <ConfigProvider
+        componentSize="large"
+        theme={{
+          token: {
+            colorPrimary: "#FFA500",
+            colorPrimaryBorder: "#FFA500",
+            colorPrimaryHover: "#b97f14",
+            colorPrimaryActive: "#FFA500",
+            colorBgContainer: "#121212",
+            colorBorder: "#d9d9d9",
+            colorPrimaryBg: "#FFA500",
+            colorText: "#d9d9d9",
+          },
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+            backgroundColor: "#121212",
+          }}
         >
-            <div
-                style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    height: '100vh',
-                    backgroundColor: '#121212',
-                }}
-            >
-                <IPConverter />
-            </div>
-        </ConfigProvider>
+          <IPConverter />
+        </div>
+      </ConfigProvider>
     );
 };
 
